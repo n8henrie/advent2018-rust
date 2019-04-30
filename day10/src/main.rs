506b3754f9549d940748f956933ef584c336f7b9
Nathan Henrie <nate@n8henrie.com>
2019-04-30 16:19:08 -0600

    Clean up comments, clippy fixes

--- day10/src/main.rs
+++ day10/src/main.rs
@@ -1,146 +1,6 @@
-// --- Day 10: The Stars Align ---
-//
-// It's no use; your navigation system simply isn't capable of providing walking directions in the arctic circle, and certainly not in 1018.
-//
-// The Elves suggest an alternative. In times like these, North Pole rescue operations will arrange points of light in the sky to guide missing Elves back to base. Unfortunately, the message is easy to miss: the points move slowly enough that it takes hours to align them, but have so much momentum that they only stay aligned for a second. If you blink at the wrong time, it might be hours before another message appears.
-//
-// You can see these points of light floating in the distance, and record their position in the sky and their velocity, the relative change in position per second (your puzzle input). The coordinates are all given from your perspective; given enough time, those positions and velocities will move the points into a cohesive message!
-//
-// Rather than wait, you decide to fast-forward the process and calculate what the points will eventually spell.
-//
-// For example, suppose you note the following points:
-//
-// position=< 9,  1> velocity=< 0,  2>
-// position=< 7,  0> velocity=<-1,  0>
-// position=< 3, -2> velocity=<-1,  1>
-// position=< 6, 10> velocity=<-2, -1>
-// position=< 2, -4> velocity=< 2,  2>
-// position=<-6, 10> velocity=< 2, -2>
-// position=< 1,  8> velocity=< 1, -1>
-// position=< 1,  7> velocity=< 1,  0>
-// position=<-3, 11> velocity=< 1, -2>
-// position=< 7,  6> velocity=<-1, -1>
-// position=<-2,  3> velocity=< 1,  0>
-// position=<-4,  3> velocity=< 2,  0>
-// position=<10, -3> velocity=<-1,  1>
-// position=< 5, 11> velocity=< 1, -2>
-// position=< 4,  7> velocity=< 0, -1>
-// position=< 8, -2> velocity=< 0,  1>
-// position=<15,  0> velocity=<-2,  0>
-// position=< 1,  6> velocity=< 1,  0>
-// position=< 8,  9> velocity=< 0, -1>
-// position=< 3,  3> velocity=<-1,  1>
-// position=< 0,  5> velocity=< 0, -1>
-// position=<-2,  2> velocity=< 2,  0>
-// position=< 5, -2> velocity=< 1,  2>
-// position=< 1,  4> velocity=< 2,  1>
-// position=<-2,  7> velocity=< 2, -2>
-// position=< 3,  6> velocity=<-1, -1>
-// position=< 5,  0> velocity=< 1,  0>
-// position=<-6,  0> velocity=< 2,  0>
-// position=< 5,  9> velocity=< 1, -2>
-// position=<14,  7> velocity=<-2,  0>
-// position=<-3,  6> velocity=< 2, -1>
-//
-// Each line represents one point. Positions are given as <X, Y> pairs: X represents how far left (negative) or right (positive) the point appears, while Y represents how far up (negative) or down (positive) the point appears.
-//
-// At 0 seconds, each point has the position given. Each second, each point's velocity is added to its position. So, a point with velocity <1, -2> is moving to the right, but is moving upward twice as quickly. If this point's initial position were <3, 9>, after 3 seconds, its position would become <6, 3>.
-//
-// Over time, the points listed above would move like this:
-//
-// Initially:
-// ........#.............
-// ................#.....
-// .........#.#..#.......
-// ......................
-// #..........#.#.......#
-// ...............#......
-// ....#.................
-// ..#.#....#............
-// .......#..............
-// ......#...............
-// ...#...#.#...#........
-// ....#..#..#.........#.
-// .......#..............
-// ...........#..#.......
-// #...........#.........
-// ...#.......#..........
-//
-// After 1 second:
-// ......................
-// ......................
-// ..........#....#......
-// ........#.....#.......
-// ..#.........#......#..
-// ......................
-// ......#...............
-// ....##.........#......
-// ......#.#.............
-// .....##.##..#.........
-// ........#.#...........
-// ........#...#.....#...
-// ..#...........#.......
-// ....#.....#.#.........
-// ......................
-// ......................
-//
-// After 2 seconds:
-// ......................
-// ......................
-// ......................
-// ..............#.......
-// ....#..#...####..#....
-// ......................
-// ........#....#........
-// ......#.#.............
-// .......#...#..........
-// .......#..#..#.#......
-// ....#....#.#..........
-// .....#...#...##.#.....
-// ........#.............
-// ......................
-// ......................
-// ......................
-//
-// After 3 seconds:
-// ......................
-// ......................
-// ......................
-// ......................
-// ......#...#..###......
-// ......#...#...#.......
-// ......#...#...#.......
-// ......#####...#.......
-// ......#...#...#.......
-// ......#...#...#.......
-// ......#...#...#.......
-// ......#...#..###......
-// ......................
-// ......................
-// ......................
-// ......................
-//
-// After 4 seconds:
-// ......................
-// ......................
-// ......................
-// ............#.........
-// ........##...#.#......
-// ......#.....#..#......
-// .....#..##.##.#.......
-// .......##.#....#......
-// ...........#....#.....
-// ..............#.......
-// ....#......#...#......
-// .....#.....##.........
-// ...............#......
-// ...............#......
-// ......................
-// ......................
-//
-// After 3 seconds, the message appeared briefly: HI. Of course, your message will be much longer and will take many more seconds to appear.
-//
-// What message will eventually appear in the sky?
+// Solution:
+// part1: EKALLKLB
+// part2: 10227
 
 use std::collections::HashSet;
 
@@ -155,20 +15,21 @@ struct Grid {
 
 impl std::fmt::Display for Grid {
     fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
-        let mut v = Vec::with_capacity((self.ymax.unwrap() - self.ymin.unwrap()) as usize);
+        let mut v = String::with_capacity(
+            (self.ymax.unwrap() - self.ymin.unwrap()) as usize
+                * (self.xmax.unwrap() - self.xmin.unwrap()) as usize,
+        );
         for y in self.ymin.unwrap()..=self.ymax.unwrap() {
-            let mut line =
-                String::with_capacity((self.xmax.unwrap() - self.xmin.unwrap()) as usize);
             for x in self.xmin.unwrap()..=self.xmax.unwrap() {
-                line.push(if self.pointset.contains(&(x, y)) {
+                v.push(if self.pointset.contains(&(x, y)) {
                     '#'
                 } else {
                     '.'
                 })
             }
-            v.push(line);
+            v.push('\n');
         }
-        write!(f, "{}", v.join("\n"))
+        write!(f, "{}", v.trim())
     }
 }
 
@@ -178,7 +39,6 @@ struct Point {
     vel: (i32, i32),
 }
 
-// position=< 3, -2> velocity=<-1,  1>
 fn parse_line(line: &str) -> Point {
     let mut nums = line
         .split(&['<', '>', ','][..])
@@ -193,36 +53,13 @@ fn parse_line(line: &str) -> Point {
     }
 }
 
-fn make_grid(points: &[Point]) -> String {
-    let xmin = points.iter().map(|p| p.pos.0).min().unwrap();
-    let ymin = points.iter().map(|p| p.pos.1).min().unwrap();
-    let xmax = points.iter().map(|p| p.pos.0).max().unwrap();
-    let ymax = points.iter().map(|p| p.pos.1).max().unwrap();
-    let adjusted_xmax = xmax - xmin;
-    let adjusted_ymax = ymax - ymin;
-
-    // dbg!((adjusted_ymax, adjusted_xmax));
-    let mut grid = vec![vec!['.'; adjusted_xmax as usize + 1]; adjusted_ymax as usize + 1];
-    // dbg!("finished initializing grid");
-    for (idx, point) in points.iter().enumerate() {
-        // dbg!(format!("working on point {}", idx));
-        let (x, y): (usize, usize) = ((point.pos.1 - ymin) as usize, (point.pos.0 - xmin) as usize);
-        grid[x][y] = '#';
-    }
-    grid.iter()
-        .map(|line| line.iter().collect::<String>())
-        .collect::<Vec<_>>()
-        .join("\n")
-}
-
-fn part1(points: &mut Vec<Point>) -> Option<String> {
+fn part1(points: &mut Vec<Point>) -> Option<(String, usize)> {
     let mut most_neighbors: Option<(usize, usize)> = None;
-    for step in 0..1_000_000 {
+    for step in 0.. {
         let mut grid = Grid {
             ..Default::default()
         };
 
-        // dbg!(format!("Step {}", step));
         for point in points.iter_mut() {
             // Set grid with points *prior* to update so that if the count of neighbors is
             // decreasing we can refer back to the grid prior to the change
@@ -243,7 +80,6 @@ fn part1(points: &mut Vec<Point>) -> Option<String> {
             grid.ymax = grid.ymax.max(Some(point.pos.1));
         }
 
-        // dbg!("counting neighbors");
         let neighbor_count = points.iter().fold(0, |acc, point| {
             acc + points
                 .iter()
@@ -253,10 +89,14 @@ fn part1(points: &mut Vec<Point>) -> Option<String> {
                 .count()
         });
         if let Some((_step, old_count)) = most_neighbors {
+            // If neighbor_count is increasing, keep going
             if neighbor_count >= old_count {
                 most_neighbors = Some((step, neighbor_count));
+
+            // Each star is double counted, and will on average have 2 neighbor stars, so only
+            // print if the neighbor_count is somewhat close to 4 times the number of stars
             } else if neighbor_count > (points.len() * 2) {
-                println!("{}", grid.to_string());
+                return Some((grid.to_string(), step));
             }
         } else {
             most_neighbors = Some((step, neighbor_count));
@@ -267,12 +107,14 @@ fn part1(points: &mut Vec<Point>) -> Option<String> {
 
 fn main() -> std::io::Result<()> {
     let input = std::fs::read_to_string("input.txt")?;
-    let points: Vec<_> = input.lines().map(|line| parse_line(line)).collect();
-    println!(
-        "part 1: {}",
-        part1(&mut points.clone()).expect("No solution found")
-    );
-    // println!("part 2: {}, part2(&input));
+    let mut points: Vec<_> = input.lines().map(|line| parse_line(line)).collect();
+
+    if let Some((stars, steps)) = part1(&mut points) {
+        println!("part 1:\n{}", stars);
+        println!("part 2: {}", steps);
+    } else {
+        println!("No solution found");
+    }
     Ok(())
 }
 
@@ -332,6 +174,6 @@ mod tests {
         .filter(|line| !line.trim().is_empty())
         .map(|line| parse_line(line))
         .collect::<Vec<_>>();
-        println!("{}", part1(&mut points).expect("No solution found"));
+        println!("{}", part1(&mut points).expect("No solution found").0);
     }
 }
