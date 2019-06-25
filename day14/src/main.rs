82d6b200ce294777d976ea70a24e8670aad32cbc
Nathan Henrie <nate@n8henrie.com>
2019-06-25 14:45:22 -0600

    Optimize part 2
    Now only checks near indices that may start the sequence of interest,
    only checks the part that may be of interest, and only converts the full
    sequence to string if the sequence is found.

--- day14/src/main.rs
+++ day14/src/main.rs
@@ -1,49 +1,5 @@
-// --- Day 14: Chocolate Charts ---
-//
-// You finally have a chance to look at all of the produce moving around. Chocolate, cinnamon, mint, chili peppers, nutmeg, vanilla... the Elves must be growing these plants to make hot chocolate! As you realize this, you hear a conversation in the distance. When you go to investigate, you discover two Elves in what appears to be a makeshift underground kitchen/laboratory.
-//
-// The Elves are trying to come up with the ultimate hot chocolate recipe; they're even maintaining a scoreboard which tracks the quality score (0-9) of each recipe.
-//
-// Only two recipes are on the board: the first recipe got a score of 3, the second, 7. Each of the two Elves has a current recipe: the first Elf starts with the first recipe, and the second Elf starts with the second recipe.
-//
-// To create new recipes, the two Elves combine their current recipes. This creates new recipes from the digits of the sum of the current recipes' scores. With the current recipes' scores of 3 and 7, their sum is 10, and so two new recipes would be created: the first with score 1 and the second with score 0. If the current recipes' scores were 2 and 3, the sum, 5, would only create one recipe (with a score of 5) with its single digit.
-//
-// The new recipes are added to the end of the scoreboard in the order they are created. So, after the first round, the scoreboard is 3, 7, 1, 0.
-//
-// After all new recipes are added to the scoreboard, each Elf picks a new current recipe. To do this, the Elf steps forward through the scoreboard a number of recipes equal to 1 plus the score of their current recipe. So, after the first round, the first Elf moves forward 1 + 3 = 4 times, while the second Elf moves forward 1 + 7 = 8 times. If they run out of recipes, they loop back around to the beginning. After the first round, both Elves happen to loop around until they land on the same recipe that they had in the beginning; in general, they will move to different recipes.
-//
-// Drawing the first Elf as parentheses and the second Elf as square brackets, they continue this process:
-//
-// (3)[7]
-// (3)[7] 1  0
-//  3  7  1 [0](1) 0
-//  3  7  1  0 [1] 0 (1)
-// (3) 7  1  0  1  0 [1] 2
-//  3  7  1  0 (1) 0  1  2 [4]
-//  3  7  1 [0] 1  0 (1) 2  4  5
-//  3  7  1  0 [1] 0  1  2 (4) 5  1
-//  3 (7) 1  0  1  0 [1] 2  4  5  1  5
-//  3  7  1  0  1  0  1  2 [4](5) 1  5  8
-//  3 (7) 1  0  1  0  1  2  4  5  1  5  8 [9]
-//  3  7  1  0  1  0  1 [2] 4 (5) 1  5  8  9  1  6
-//  3  7  1  0  1  0  1  2  4  5 [1] 5  8  9  1 (6) 7
-//  3  7  1  0 (1) 0  1  2  4  5  1  5 [8] 9  1  6  7  7
-//  3  7 [1] 0  1  0 (1) 2  4  5  1  5  8  9  1  6  7  7  9
-//  3  7  1  0 [1] 0  1  2 (4) 5  1  5  8  9  1  6  7  7  9  2
-//
-// The Elves think their skill will improve after making a few recipes (your puzzle input). However, that could take ages; you can speed this up considerably by identifying the scores of the ten recipes after that. For example:
-//
-//     If the Elves think their skill will improve after making 9 recipes, the scores of the ten recipes after the first nine on the scoreboard would be 5158916779 (highlighted in the last line of the diagram).
-//     After 5 recipes, the scores of the next ten would be 0124515891.
-//     After 18 recipes, the scores of the next ten would be 9251071085.
-//     After 2018 recipes, the scores of the next ten would be 5941429882.
-//
-// What are the scores of the ten recipes immediately after the number of recipes in your puzzle input?
-//
-// Your puzzle input is 846021.
-// part 1: 5482326119
-// part 2 is not 846021
 use std::char;
+use std::collections::VecDeque;
 
 fn part1(input: u32) -> String {
     let (mut first_elf, mut second_elf) = ((0_usize, 3_u8), (1_usize, 7_u8));
@@ -85,13 +41,33 @@ fn part2(input: &str) -> usize {
         char::from_digit(first_elf.1.into(), 10).unwrap(),
         char::from_digit(second_elf.1.into(), 10).unwrap(),
     ];
+    let mut check_queue = VecDeque::new();
+    let start_char = input.chars().next().unwrap();
 
     loop {
-        let new_recipe = first_elf.1 + second_elf.1;
-        full_recipe.extend(new_recipe.to_string().chars());
+        let new_recipe: Vec<_> = (first_elf.1 + second_elf.1).to_string().chars().collect();
+        if new_recipe.contains(&start_char) {
+            check_queue.push_back(full_recipe.len() - 1);
+        }
 
-        if let Some(idx) = full_recipe.iter().collect::<String>().find(input) {
-            return idx;
+        full_recipe.extend(new_recipe);
+
+        if let Some(idx) = check_queue.get(0) {
+            if full_recipe.len().checked_sub(input.len()).unwrap_or(0) > *idx {
+                {
+                    check_queue.pop_front();
+
+                    if full_recipe
+                        .get(full_recipe.len() - input.len() - 2..)
+                        .unwrap()
+                        .iter()
+                        .collect::<String>()
+                        .contains(input)
+                    {
+                        return full_recipe.iter().collect::<String>().find(input).unwrap();
+                    }
+                }
+            }
         }
 
         let first_elf_idx = (first_elf.0 + first_elf.1 as usize + 1) % full_recipe.len();
@@ -109,7 +85,7 @@ fn part2(input: &str) -> usize {
 
 fn main() {
     println!("part 1: {}", part1(846_021));
-    println!("part 2: {}", part2("846_021"));
+    println!("part 2: {}", part2("846021"));
 }
 
 #[cfg(test)]
