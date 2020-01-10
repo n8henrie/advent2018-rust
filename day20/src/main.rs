cb30eff715dc93df008647e91e0c886b6756fbdc
Nathan Henrie <nate@n8henrie.com>
2020-01-10 09:48:54 -0700

    Add part2, clean up with clippy

--- day20/src/main.rs
+++ day20/src/main.rs
@@ -1,204 +1,3 @@
-// --- Day 20: A Regular Map ---
-//
-// While you were learning about instruction pointers, the Elves made
-// considerable progress. When you look up, you discover that the North Pole
-// base construction project has completely surrounded you.
-//
-// The area you are in is made up entirely of rooms and doors. The rooms are
-// arranged in a grid, and rooms only connect to adjacent rooms when a door is
-// present between them.
-//
-// For example, drawing rooms as ., walls as #, doors as | or -, your current
-// position as X, and where north is up, the area you're in might look like
-// this:
-//
-// #####
-// #.|.#
-// #-###
-// #.|X#
-// #####
-//
-// You get the attention of a passing construction Elf and ask for a map. "I
-// don't have time to draw out a map of this place - it's huge. Instead, I can
-// give you directions to every room in the facility!" He writes down some
-// directions on a piece of parchment and runs off. In the example above, the
-// instructions might have been ^WNE$, a regular expression or "regex" (your
-// puzzle input).
-//
-// The regex matches routes (like WNE for "west, north, east") that will take
-// you from your current room through various doors in the facility. In
-// aggregate, the routes will take you through every door in the facility at
-// least once; mapping out all of these routes will let you build a proper map
-// and find your way around.
-//
-// ^ and $ are at the beginning and end of your regex; these just mean that the
-// regex doesn't match anything outside the routes it describes. (Specifically,
-// ^ matches the start of the route, and $ matches the end of it.) These
-// characters will not appear elsewhere in the regex.
-//
-// The rest of the regex matches various sequences of the characters N (north),
-// S (south), E (east), and W (west). In the example above, ^WNE$ matches only
-// one route, WNE, which means you can move west, then north, then east from
-// your current position. Sequences of letters like this always match that exact
-// route in the same order.
-//
-// Sometimes, the route can branch. A branch is given by a list of options
-// separated by pipes (|) and wrapped in parentheses. So, ^N(E|W)N$ contains a
-// branch: after going north, you must choose to go either east or west before
-// finishing your route by going north again. By tracing out the possible routes
-// after branching, you can determine where the doors are and, therefore, where
-// the rooms are in the facility.
-//
-// For example, consider this regex: ^ENWWW(NEEE|SSE(EE|N))$
-//
-// This regex begins with ENWWW, which means that from your current position,
-// all routes must begin by moving east, north, and then west three times, in
-// that order. After this, there is a branch. Before you consider the branch,
-// this is what you know about the map so far, with doors you aren't sure about
-// marked with a ?:
-//
-// #?#?#?#?#
-// ?.|.|.|.?
-// #?#?#?#-#
-//     ?X|.?
-//     #?#?#
-//
-// After this point, there is (NEEE|SSE(EE|N)). This gives you exactly two
-// options: NEEE and SSE(EE|N). By following NEEE, the map now looks like this:
-//
-// #?#?#?#?#
-// ?.|.|.|.?
-// #-#?#?#?#
-// ?.|.|.|.?
-// #?#?#?#-#
-//     ?X|.?
-//     #?#?#
-//
-// Now, only SSE(EE|N) remains. Because it is in the same parenthesized group as
-// NEEE, it starts from the same room NEEE started in. It states that starting
-// from that point, there exist doors which will allow you to move south twice,
-// then east; this ends up at another branch. After that, you can either move
-// east twice or north once. This information fills in the rest of the doors:
-//
-// #?#?#?#?#
-// ?.|.|.|.?
-// #-#?#?#?#
-// ?.|.|.|.?
-// #-#?#?#-#
-// ?.?.?X|.?
-// #-#-#?#?#
-// ?.|.|.|.?
-// #?#?#?#?#
-//
-// Once you've followed all possible routes, you know the remaining unknown
-// parts are all walls, producing a finished map of the facility:
-//
-// #########
-// #.|.|.|.#
-// #-#######
-// #.|.|.|.#
-// #-#####-#
-// #.#.#X|.#
-// #-#-#####
-// #.|.|.|.#
-// #########
-//
-// Sometimes, a list of options can have an empty option, like (NEWS|WNSE|).
-// This means that routes at this point could effectively skip the options in
-// parentheses and move on immediately. For example, consider this regex and the
-// corresponding map:
-//
-// ^ENNWSWW(NEWS|)SSSEEN(WNSE|)EE(SWEN|)NNN$
-//
-// ###########
-// #.|.#.|.#.#
-// #-###-#-#-#
-// #.|.|.#.#.#
-// #-#####-#-#
-// #.#.#X|.#.#
-// #-#-#####-#
-// #.#.|.|.|.#
-// #-###-###-#
-// #.|.|.#.|.#
-// ###########
-//
-// This regex has one main route which, at three locations, can optionally
-// include additional detours and be valid: (NEWS|), (WNSE|), and (SWEN|).
-// Regardless of which option is taken, the route continues from the position it
-// is left at after taking those steps. So, for example, this regex matches all
-// of the following routes (and more that aren't listed here):
-//
-//     ENNWSWWSSSEENEENNN
-//     ENNWSWWNEWSSSSEENEENNN
-//     ENNWSWWNEWSSSSEENEESWENNNN
-//     ENNWSWWSSSEENWNSEEENNN
-//By following the various routes the regex matches, a full map of all of the
-//doors and rooms in the facility can be assembled.
-//
-//To get a sense for the size of this facility, you'd like to determine which
-//room is furthest from you: specifically, you would like to find the room for
-//which the shortest path to that room would require passing through the most
-//doors.
-//
-//     In the first example (^WNE$), this would be the north-east corner 3 doors
-//     away. In the second example (^ENWWW(NEEE|SSE(EE|N))$), this would be the
-//     south-east corner 10 doors away. In the third example
-//     (^ENNWSWW(NEWS|)SSSEEN(WNSE|)EE(SWEN|)NNN$), this would be the north-east
-//     corner 18 doors away.
-//
-// Here are a few more examples:
-//
-// Regex: ^ESSWWN(E|NNENN(EESS(WNSE|)SSS|WWWSSSSE(SW|NNNE)))$
-// Furthest room requires passing 23 doors
-//
-// #############
-// #.|.|.|.|.|.#
-// #-#####-###-#
-// #.#.|.#.#.#.#
-// #-#-###-#-#-#
-// #.#.#.|.#.|.#
-// #-#-#-#####-#
-// #.#.#.#X|.#.#
-// #-#-#-###-#-#
-// #.|.#.|.#.#.#
-// ###-#-###-#-#
-// #.|.#.|.|.#.#
-// #############
-//
-// Regex: ^WSSEESWWWNW(S|NENNEEEENN(ESSSSW(NWSW|SSEN)|WSWWN(E|WWS(E|SS))))$
-
-// Example: ^WSSEESWWWNW(S|NENNEEEENN(ESSSSW(NWSW|SSEN)|WSWWN(E|WWS(E|SS)))N)$
-// WSSEESWWWNW
-// S  |     NENNEEEENN
-//      ESSSSW      |    WSWWN
-//    NWSW | SSEN        E | WWS
-//                           E | SS
-
-// Furthest room requires passing 31 doors
-//
-// ###############
-// #.|.|.|.#.|.|.#
-// #-###-###-#-#-#
-// #.|.#.|.|.#.#.#
-// #-#########-#-#
-// #.#.|.|.|.|.#.#
-// #-#-#########-#
-// #.#.#.|X#.|.#.#
-// ###-#-###-#-#-#
-// #.|.#.#.|.#.|.#
-// #-###-#####-###
-// #.|.#.|.|.#.#.#
-// #-#-#####-#-#-#
-// #.#.|.|.|.#.|.#
-// ###############
-//
-// What is the largest number of doors you would be required to pass through to
-// reach a room? That is, find the room for which the shortest path from your
-// starting location to that room would require passing through the most doors;
-// what is the fewest doors you can pass through to reach it?
-//
-// part1: 4778
-
 use std::cell::RefCell;
 use std::collections::{HashMap, HashSet};
 use std::convert::TryInto;
@@ -313,9 +112,6 @@ impl Area {
             ymax: (ymax - ymin + 3).try_into().unwrap(),
         }
     }
-    fn longest(&self) -> u32 {
-        *self.grid.values().map(|(_, count)| count).max().unwrap()
-    }
 }
 
 impl fmt::Display for Gridpoint {
@@ -400,22 +196,6 @@ impl Node {
         }
         recurse(node, Vec::new())
     }
-
-    fn longest(node: &Rc<RefCell<Node>>) -> usize {
-        fn recurse(node: &Rc<RefCell<Node>>, steps: usize) -> Vec<usize> {
-            let len = node.borrow().directions.len();
-            if node.borrow().next.is_empty() {
-                vec![steps + len]
-            } else {
-                let mut paths = Vec::new();
-                for n in node.borrow().next.iter() {
-                    paths.extend(recurse(n, steps + len))
-                }
-                paths
-            }
-        }
-        recurse(node, 0).into_iter().max().unwrap()
-    }
 }
 
 enum Gridpoint {
@@ -442,15 +222,20 @@ fn dedup_rc<T>(iter: T) -> Vec<Rc<RefCell<Node>>>
 where
     T: Iterator<Item = Rc<RefCell<Node>>>,
 {
-    let hs: HashSet<NodeHasher> = HashSet::from_iter(iter.into_iter().map(|node| NodeHasher(node)));
+    let hs: HashSet<NodeHasher> = HashSet::from_iter(iter.map(NodeHasher));
     hs.into_iter().map(|n| n.0).collect::<Vec<_>>()
 }
 
-#[derive(Eq, PartialEq)]
+#[derive(Eq)]
 struct NodeHasher(Rc<RefCell<Node>>);
 impl Hash for NodeHasher {
     fn hash<H: Hasher>(&self, state: &mut H) {
         self.0.as_ptr().hash(state);
+    }
+}
+impl PartialEq for NodeHasher {
+    fn eq(&self, other: &NodeHasher) -> bool {
+        self.0.as_ptr().eq(&other.0.as_ptr())
     }
 }
 
@@ -526,7 +311,6 @@ impl From<char> for Direction {
             'E' => Direction::E,
             'S' => Direction::S,
             'W' => Direction::W,
-            // _ => panic!(format!("bad character: {}", c)),
             _ => unreachable!(),
         }
     }
@@ -534,8 +318,15 @@ impl From<char> for Direction {
 
 fn part1(input: &str) -> u32 {
     let area = Area::from_regex(input);
-    // println!("{}", area.to_string());
-    area.longest()
+    *area.grid.values().map(|(_, count)| count).max().unwrap()
+}
+
+fn part2(input: &str) -> usize {
+    let area = Area::from_regex(input);
+    area.grid
+        .values()
+        .filter(|(_, count)| *count >= 1000)
+        .count()
 }
 
 fn main() -> Result<()> {
@@ -543,6 +334,7 @@ fn main() -> Result<()> {
     let input = input.trim();
 
     println!("part1: {}", part1(&input));
+    println!("part2: {}", part2(&input));
     Ok(())
 }
 
