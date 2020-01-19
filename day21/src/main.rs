1195bf6b8b93af2d981d4a5e95200c96b9495481
Nathan Henrie <nate@n8henrie.com>
2020-01-19 16:57:24 -0700

    Initial commit

--- day21/src/main.rs
+++ day21/src/main.rs
@@ -1,45 +1,199 @@
-// --- Day 21: Chronal Conversion ---
-//
-// You should have been watching where you were going, because as you wander the new North Pole
-// base, you trip and fall into a very deep hole!
-//
-// Just kidding. You're falling through time again.
-//
-// If you keep up your current pace, you should have resolved all of the temporal anomalies by the
-// next time the device activates. Since you have very little interest in browsing history in
-// 500-year increments for the rest of your life, you need to find a way to get back to your
-// present time.
-//
-// After a little research, you discover two important facts about the behavior of the device:
-//
-// First, you discover that the device is hard-wired to always send you back in time in 500-year
-// increments. Changing this is probably not feasible.
-//
-// Second, you discover the activation system (your puzzle input) for the time travel module.
-// Currently, it appears to run forever without halting.
-//
-// If you can cause the activation system to halt at a specific moment, maybe you can make the
-// device send you so far back in time that you cause an integer underflow in time itself and wrap
-// around back to your current time!
-//
-// The device executes the program as specified in manual section one and manual section two.
-//
-// Your goal is to figure out how the program works and cause it to halt. You can only control
-// register 0; every other register begins at 0 as usual.
-//
-// Because time travel is a dangerous activity, the activation system begins with a few
-// instructions which verify that bitwise AND (via bani) does a numeric operation and not an
-// operation as if the inputs were interpreted as strings. If the test fails, it enters an infinite
-// loop re-running the test instead of allowing the program to execute normally. If the test
-// passes, the program continues, and assumes that all other bitwise operations (banr, bori, and
-// borr) also interpret their inputs as numbers. (Clearly, the Elves who wrote this system were
-// worried that someone might introduce a bug while trying to emulate this system with a scripting
-// language.)
-//
-// What is the lowest non-negative integer value for register 0 that causes the program to halt
-// after executing the fewest instructions? (Executing the same instruction multiple times counts
-// as multiple instructions executed.)
+use std::collections::HashSet;
+use std::error;
+use std::io::{Error, ErrorKind};
 
-fn main() {
-    println!("Hello, world!");
+type Result<T> = std::result::Result<T, Box<dyn error::Error>>;
+
+type Arguments = [usize; 3];
+
+/// addi (add immediate) stores into register C the result of adding register A and value B.
+fn addi(command: &Arguments, input: &mut [usize; 6]) {
+    input[command[2]] = input[command[0]] + command[1];
+}
+
+/// addr (add register) stores into register C the result of adding register A and register B.
+fn addr(command: &Arguments, input: &mut [usize; 6]) {
+    input[command[2]] = input[command[0]] + input[command[1]];
+}
+
+/// (multiply register) stores into register C the result of multiplying register A and register B.
+fn mulr(command: &Arguments, input: &mut [usize; 6]) {
+    input[command[2]] = input[command[0]] * input[command[1]];
+}
+
+/// muli (multiply immediate) stores into register C the result of multiplying register A and value B.
+fn muli(command: &Arguments, input: &mut [usize; 6]) {
+    input[command[2]] = input[command[0]] * command[1];
+}
+
+/// banr (bitwise AND register) stores into register C the result of the bitwise AND of register A and register B.
+fn banr(command: &Arguments, input: &mut [usize; 6]) {
+    input[command[2]] = input[command[0]] & input[command[1]];
+}
+
+/// bani (bitwise AND immediate) stores into register C the result of the bitwise AND of register A and value B.
+fn bani(command: &Arguments, input: &mut [usize; 6]) {
+    input[command[2]] = input[command[0]] & command[1];
+}
+
+/// borr (bitwise OR register) stores into register C the result of the bitwise OR of register A and register B.
+fn borr(command: &Arguments, input: &mut [usize; 6]) {
+    input[command[2]] = input[command[0]] | input[command[1]];
+}
+
+/// bori (bitwise OR immediate) stores into register C the result of the bitwise OR of register A and value B.
+fn bori(command: &Arguments, input: &mut [usize; 6]) {
+    input[command[2]] = input[command[0]] | command[1];
+}
+
+/// setr (set register) copies the contents of register A into register C. (Input B is ignored.)
+fn setr(command: &Arguments, input: &mut [usize; 6]) {
+    input[command[2]] = input[command[0]];
+}
+
+/// seti (set immediate) stores value A into register C. (Input B is ignored.)
+fn seti(command: &Arguments, input: &mut [usize; 6]) {
+    input[command[2]] = command[0];
+}
+
+/// gtir (greater-than immediate/register) sets register C to 1 if value A is greater than register B. Otherwise, register C is set to 0.
+fn gtir(command: &Arguments, input: &mut [usize; 6]) {
+    input[command[2]] = if command[0] > input[command[1]] { 1 } else { 0 };
+}
+
+/// gtri (greater-than register/immediate) sets register C to 1 if register A is greater than value B. Otherwise, register C is set to 0.
+fn gtri(command: &Arguments, input: &mut [usize; 6]) {
+    input[command[2]] = if input[command[0]] > command[1] { 1 } else { 0 };
+}
+
+/// gtrr (greater-than register/register) sets register C to 1 if register A is greater than register B. Otherwise, register C is set to 0.
+fn gtrr(command: &Arguments, input: &mut [usize; 6]) {
+    input[command[2]] = if input[command[0]] > input[command[1]] {
+        1
+    } else {
+        0
+    };
+}
+
+/// eqir (equal immediate/register) sets register C to 1 if value A is equal to register B. Otherwise, register C is set to 0.
+fn eqir(command: &Arguments, input: &mut [usize; 6]) {
+    input[command[2]] = if command[0] == input[command[1]] {
+        1
+    } else {
+        0
+    };
+}
+
+/// eqri (equal register/immediate) sets register C to 1 if register A is equal to value B. Otherwise, register C is set to 0.
+fn eqri(command: &Arguments, input: &mut [usize; 6]) {
+    input[command[2]] = if input[command[0]] == command[1] {
+        1
+    } else {
+        0
+    };
+}
+
+/// eqrr (equal register/register) sets register C to 1 if register A is equal to register B. Otherwise, register C is set to 0.
+fn eqrr(command: &Arguments, input: &mut [usize; 6]) {
+    input[command[2]] = if input[command[0]] == input[command[1]] {
+        1
+    } else {
+        0
+    };
+}
+
+struct Program<'a> {
+    ip: usize,
+    cmds: Vec<(&'a str, [usize; 3])>,
+}
+
+impl<'a> Program<'a> {
+    // Looking at the input, step 28 (line 30) is the only place that register[0] is even looked
+    // at, and it isn't set anywhere. If register[4] == register[0] at step 28, the program will
+    // end due to step 29, which will skip step 30, so we will just return the value of register[4]
+    // the first time step 28 is reached.
+    fn run(&self, regzero: usize) -> Result<(Option<usize>, Option<usize>)> {
+        let ip = self.ip;
+        let mut register: [usize; 6] = [0; 6];
+        register[0] = regzero;
+
+        let mut seen = HashSet::new();
+        let mut fourset = HashSet::new();
+        let mut fours = (None, None);
+
+        while let Some((progname, args)) = self.cmds.get(register[ip]) {
+            if register[ip] == 18 && ((register[2] + 1) * 256) <= register[3] {
+                register[1] = 0;
+                register[2] = register[3] / 256;
+                register[5] = 18;
+                continue;
+            }
+            let prog = match *progname {
+                "addi" => addi,
+                "addr" => addr,
+                "bani" => bani,
+                "bori" => bori,
+                "eqri" => eqri,
+                "eqrr" => eqrr,
+                "gtir" => gtir,
+                "gtrr" => gtrr,
+                "muli" => muli,
+                "seti" => seti,
+                "setr" => setr,
+                _ => unreachable!(),
+            };
+            prog(args, &mut register);
+            register[ip] += 1;
+            if register[ip] == 28 && fourset.insert(register[4]) {
+                fours = match fours {
+                    (None, _) => (Some(register[4]), None),
+                    (Some(num), _) => (Some(num), Some(register[4])),
+                }
+            }
+            if !seen.insert(register) {
+                return Ok(fours);
+            }
+        }
+        Err(Box::new(Error::new(ErrorKind::Other, "Out of bounds")))
+    }
+}
+
+fn main() -> Result<()> {
+    let input = std::fs::read_to_string("input.txt")?;
+    let mut lines = input.lines();
+    let ip = lines
+        .next()
+        .unwrap()
+        .split_whitespace()
+        .last()
+        .unwrap()
+        .parse::<usize>()
+        .unwrap();
+    let program = Program {
+        ip,
+        cmds: lines
+            .map(|line| {
+                let mut parts = line.split_whitespace();
+                if let (Some(cmd), Some(a), Some(b), Some(c)) =
+                    (parts.next(), parts.next(), parts.next(), parts.next())
+                {
+                    (
+                        cmd,
+                        [a.parse().unwrap(), b.parse().unwrap(), c.parse().unwrap()],
+                    )
+                } else {
+                    panic!("Parse error!")
+                }
+            })
+            .collect(),
+    };
+    let solution = program.run(0)?;
+    println!("part1: {}", solution.0.unwrap());
+    println!("part2: {}", solution.1.unwrap());
+    Ok(())
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
 }
